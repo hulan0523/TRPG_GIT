9bd557fddcdb7ea9ae20897f1feda6ffa971f4df
hulan <kxshadow0523@gmail.com>
2017-05-13 12:49:03 +0800

    save

---
+++ TRPG_8/Assets/Script/soup_event.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soup_event : MonoBehaviour {
+
+    Text MsgText;
+
+
+    public void point_click()
+    {
+        GameObject.Find("MsgCanvas").GetComponent<Canvas>().enabled = true;
+        MsgText = GameObject.Find("MsgText").GetComponent<Text>();
+        MsgText.text = "這個湯有毒\n";
+        MsgText.text = "進行靈感判定";
+        GameObject.Find("dice").GetComponent<dice>().enabled = true;
+        dice dice = new dice();
+        MsgText.text = "擲到" + dice.ThrowNum();
+    }
+}
---
+++ TRPG_8/Assets/dice.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class dice : MonoBehaviour {
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public int ThrowNum()
+    {
+        anim = GetComponent<Animator>();
+        int r = Random.Range(1, 7);
+        if (r == 1)
+        {
+            anim.SetInteger("diceNum", 1);
+            Debug.Log("骰到1");
+        }
+        else if (r == 2)
+        {
+            anim.SetInteger("diceNum", 2);
+            Debug.Log("骰到2");
+        }
+        else if (r == 3)
+        {
+            anim.SetInteger("diceNum", 3);
+            Debug.Log("骰到3");
+        }
+        else if (r == 4)
+        {
+            anim.SetInteger("diceNum", 4);
+            Debug.Log("骰到4");
+        }
+        else if (r == 5)
+        {
+            anim.SetInteger("diceNum", 5);
+            Debug.Log("骰到5");
+        }
+        else if (r == 6)
+        {
+            anim.SetInteger("diceNum", 6);
+            Debug.Log("骰到6");
+        }
+        return r;
+    }
+}
