e21ae2b8020082bf8a2d358740974db74f94c22e
hulan0523 <kxshadow0523@gmail.com>
2017-05-19 09:34:24 +0800

    more open

--- TRPG_8/Assets/Script/survey.cs
+++ TRPG_8/Assets/Script/survey.cs
@@ -29,9 +29,47 @@ public class survey : MonoBehaviour {
     public void Open_OnClick()
     {
         string focusingObject = PlayerPrefs.GetString("FocusAt");
-        PlayerPrefs.SetString("Room","B");//利用substring直接抓去了哪裡
-        GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
-    }
+        if(focusingObject == "BtoA" )
+        {
+            PlayerPrefs.SetString("Room","A");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "CtoA" )
+        {
+            PlayerPrefs.SetString("Room","A");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "DtoA" )
+        {
+            PlayerPrefs.SetString("Room","A");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "EtoA" )
+        {
+            PlayerPrefs.SetString("Room","A");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "AtoB" )//去左邊房間
+        {
+            PlayerPrefs.SetString("Room","B");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "AtoC" //去上面房間
+        {
+            PlayerPrefs.SetString("Room","C");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "AtoD") //去右邊房間
+        {
+            PlayerPrefs.SetString("Room","D");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+        else if(focusingObject == "AtoE") //去下面房間
+        {
+            PlayerPrefs.SetString("Room","E");
+            GameObject.Find("ME").transform.position = new Vector3(-4f,0f,0f);
+        }
+    } 
 
     [Command]
     void CmdPickObject()
